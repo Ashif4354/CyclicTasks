063faba80aa02bb11581ee323247d115c4845b6b
Ashif4354 <ashif.abhas@gmail.com>
2024-09-29 10:23:16 +0530

    Refactor project setup instructions, Dockerignore files, header import, package.json, and frontend files

---
+++ Frontend/src/Pages/Admin/Admin.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+
+const Admin = () => {
+
+    const [user, setUser] = useState(null);
+
+    return (
+        <div className='main-container'>
+        </div>
+    );
+}
+
+export default Admin;
